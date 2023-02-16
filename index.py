08cbb386d133ecc2795b85bf45499c5b35398f45
Juamir Garcia <juamir@hamael.org>
2023-02-16 18:03:52 +0100

    CRUD FAST API

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
-#Imports
+# Imports
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
 
-#Create app
+client_apps = ['http://localhost:3000']  # Our REACT app will be running on this IP and PORT
+
+# Create app
 app = FastAPI()
 
-#Register your router
+# Register your router
 app.include_router(student_router)
+
+# Register App with CORS middleware to allow resource shsaring between different domians/origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_apps,
+    # allow_credential=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
